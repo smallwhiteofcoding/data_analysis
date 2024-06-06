6518cf8816b6d0f4beefdd71193ac861eacec3a6
MSN8080 <2539770047@QQ.com>
2024-06-06 10:42:00 +0800

    msn

--- dual_view_review_sum-master/utils/time_log.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def time_since(start_time):
-    return time.time()-start_time
-
-if __name__ == '__main__':
-    start_time = time.time()
-    time.sleep(5)
-    print(time_since(start_time))
